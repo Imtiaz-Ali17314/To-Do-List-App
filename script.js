fadd0dfdb58f1bb4749dfa6bd50c47d47952f21f
Abbas Raza <abbas.inwork@gmail.com>
2025-10-21 19:48:26 +0500

    To Do List App using js and local storage

---
+++ script.js
@@ -0,0 +1,40 @@
+const searchBox = document.querySelector("#searchBox");
+const listContainer = document.querySelector(".list-container");
+
+function addTask() {
+  if (searchBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = searchBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  searchBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showDta() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showDta();
